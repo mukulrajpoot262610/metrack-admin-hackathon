697a6bb91629aa2dfe9e26927f6fe2b165a16418
mukulrajpoot262610 <mukulrajpoot262610@gmail.com>
2022-06-18 18:24:04 +0530

    ADD: redux

---
+++ redux/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    isAuth: false,
+    user: null,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setAuth: (state, action) => {
+            const { user } = action.payload
+            state.user = user
+            if (user === null) {
+                state.isAuth = false
+            } else {
+                state.isAuth = true
+            }
+        },
+        purchaseCourse: (state, action) => {
+            state.user.coursePurchased = [...state.user.coursePurchased, action.payload]
+        }
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { setAuth, purchaseCourse } = authSlice.actions
+
+export default authSlice.reducer
