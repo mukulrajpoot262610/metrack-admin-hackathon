ca8e2723d458cd3c2541c6ce1440399a774e66a0
Amit Kumar <alsoamit@gmail.com>
2022-06-29 13:20:54 +0530

    refactor: users

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -11,30 +11,35 @@ import Login from "../../pages";
 
 const Layout = ({ children }) => {
   const router = useRouter();
-  const { isAuth } = useSelector(state => state.auth)
+  const { isAuth } = useSelector((state) => state.auth);
   const { loading } = useLoadingWithRefresh();
 
   if (loading) {
-    return (<Loader />)
+    return <Loader />;
   }
 
   if (!isAuth) {
-    (router.pathname.includes('/account') || router.pathname.includes('/courses') || router.pathname.includes('/users')) && router.replace('/')
-    return <>
-      <Toaster />
-      <Login />
-    </>
+    (router.pathname.includes("/account") ||
+      router.pathname.includes("/courses") ||
+      router.pathname.includes("/users")) &&
+      router.replace("/");
+    return (
+      <>
+        <Toaster />
+        <Login />
+      </>
+    );
   }
 
   if (isAuth) {
-    (router.pathname === '/' && router.replace('/account'))
+    router.pathname === "/" && router.replace("/account");
     return (
       <>
         {router.pathname !== "/" && <Navbar />}
         <Toaster />
         <main className="flex flex-col items-center justify-center w-full">
           <Head>
-            <title>MentorMap Admin Panel</title>
+            <title>Metrack Admin Panel</title>
           </Head>
           <div className="flex w-full">
             {router.pathname !== "/" &&
@@ -44,7 +49,7 @@ const Layout = ({ children }) => {
                 </div>
               )}
             {router.pathname !== "/" &&
-              router.pathname !== "/auth/forget-password" ? (
+            router.pathname !== "/auth/forget-password" ? (
               <div className="w-full mt-16 lg:w-10/12">{children}</div>
             ) : (
               <div className="w-full pt-16">{children}</div>
@@ -52,9 +57,8 @@ const Layout = ({ children }) => {
           </div>
         </main>
       </>
-    )
-
+    );
   }
-}
+};
 
 export default Layout;
--- components/users/Table.jsx
+++ components/users/Table.jsx
@@ -1,3 +1,4 @@
+import { formatDistance, format } from "date-fns";
 import React from "react";
 
 export default function Table({ data, handleDelete, loading }) {
@@ -13,31 +14,38 @@ export default function Table({ data, handleDelete, loading }) {
           <div className="flex items-center space-x-3">
             <div className="avatar">
               <div className="w-12 h-12 mask mask-squircle">
-                <img src="avatar.svg" alt="Avatar Tailwind CSS Component" />
+                <img src={i?.avatar || "/avatar.svg"} alt="user avatar" />
               </div>
             </div>
             <div>
               <div className="font-bold">{i?.name}</div>
-              <div className="text-sm opacity-50">{i?.email}</div>
+              <div className="flex flex-col text-sm opacity-70">
+                <p className="text-yellow-400">@{i?.username}</p>
+                <p className="">{i?.email}</p>
+              </div>
             </div>
           </div>
         </td>
-        <td>
-          User since 1949
+        <td className="text-sm">
+          joined {formatDistance(new Date(i?.createdAt), Date.now())} ago joined{" "}
           <br />
-          <span
-            className={`badge badge-sm ${i?.role > 1 ? "badge-success" : "badge-ghost"
-              }`}
-          >
-            {i?.role > 1 ? "admin" : "user"}
-          </span>
+          <p className="pt-2 text-opacity-80 text-base-content">
+            {format(new Date(i?.createdAt), "dd MM yyyy")}
+          </p>
         </td>
-        <td>
+        <td className="flex flex-col justify-center gap-y-4">
           {i?.verified ? (
             <p className="font-bold text-green-500">verified</p>
           ) : (
             <p>unverified</p>
           )}
+          <span
+            className={`badge badge-sm ${
+              i?.role > 1 ? "badge-success" : "badge-ghost"
+            }`}
+          >
+            {i?.role > 1 ? "admin" : "user"}
+          </span>
         </td>
         <th>
           <button
